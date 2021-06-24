99739a99605e3297b2bdb659751e4e0d3b4a7835
Mikhail Podalyako <mihailpodalyako1@gmail.com>
2021-06-24 20:25:42 +0500

    почему то падает.

--- app/src/main/java/com/gmail/notebook2/ui/MainActivity.java
+++ app/src/main/java/com/gmail/notebook2/ui/MainActivity.java
@@ -1,8 +1,17 @@
 package com.gmail.notebook2.ui;
 
+import androidx.annotation.NonNull;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
 
 import com.gmail.notebook2.R;
 
@@ -12,5 +21,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appnar_menu,menu);
+
+        MenuItem actionSearch = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) actionSearch.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, "Query", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.option_own){
+            Toast.makeText(this, "Option One Selected", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (itemId == R.id.option_two){
+            Toast.makeText(this, "Option Two Selected", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (itemId == R.id.option_three){
+            Toast.makeText(this, "Option Tree Selected", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return true;
     }
 }
